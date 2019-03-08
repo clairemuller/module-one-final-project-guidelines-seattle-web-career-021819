c55820ff8a0ed6d6c622f66be3ec2513eb0e1bae
clairemuller <clairermuller@gmail.com>
2019-03-08 10:48:14 -0800

    merged 03 and wrote README

--- lib/03_most_favorite_recipes.rb
+++ lib/03_most_favorite_recipes.rb
@@ -1,42 +1,33 @@
-# helper that sorts all favorites by count
-def sort
-  count = Favorite.all.group(:recipe_id).count
-  sorted = count.sort_by { |k, v| v }.reverse
-  sorted
-end
-
 def most_favorite_recipes
-  sorted_favorites = sort
+  system "clear"
+  results = Favorite.all.group(:recipe_id).count
+  results = results.sort_by {|k,v| v}.reverse
   puts
-  puts "There are #{sorted_favorites.count} recipes favorited."
-  puts "How many of these recipes would you like to see?"
-  choice = gets.chomp.to_i
-  if choice > sorted_favorites.count
+  puts "There are #{results.count} recipes favorited."
+  print "How many recipes would you like to see? "
+  input = gets.chomp.to_i
+
+  if input > results.count
     puts
-    puts "Oops! We only have #{sorted_favorites.count} recipes favorited!"
-    puts "Here they are:"
-    choice = sorted_favorites.count
-    show_favorites(choice)
-  else
-    show_favorites(choice)
+    puts "Oops! We only have #{results.count} recipes favorited!"
+    input = results.count
   end
+
+  show_favorites(results, input)
+  sleep(3)
+  puts
+  puts "Screen will clear in 5 seconds"
+  sleep(5)
+  system "clear"
+  main_menu
 end
 
-def show_favorites(choice)
-  sorted_favorites = sort
-  distinct_faves = Favorite.all.select(:recipe_id).distinct
-  i = 1
-
-  distinct_faves.all.each do |xx|
-    sorted_favorites[0...choice].find do |yy|
-      if xx.recipe_id == yy[0]
-        puts
-        puts "##{i}: #{xx.recipe.name}"
-        i += 1
-      end
-    end
-  end
+def show_favorites(results, input)
   puts
-  puts "Returning to main menu..."
-  main_menu
+  puts "The top #{input} recipes are:"
+  results[0...input].each_with_index do |result, i|
+    recipe = Recipe.find(result[0])
+    puts "#{i+1}. #{recipe.name}"
+    sleep(0.5)
+  end
 end
